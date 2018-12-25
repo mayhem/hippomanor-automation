b381815f2e24082cea9d732b9672895244c31093
Robert Kaye <rob@musicbrainz.org>
2018-12-25 15:57:24 -0800

    Added a temperature/humidity logger based on the DHT11 shield board.

---
+++ mqtt_snooper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/evn python
+import paho.mqtt.client as mqtt
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+# If you want to use a specific client id, use
+# mqttc = mqtt.Client("mqtt-snooper")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.connect("10.1.1.105", 1883, 60)
+mqttc.subscribe("#", 0)
+mqttc.loop_forever()
---
+++ temp_sensor.py
@@ -0,0 +1,87 @@
+from math import fmod
+from time import sleep, time
+import ubinascii
+import machine
+import dht
+import network
+import json
+from umqtt.simple import MQTTClient
+
+import config
+
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+
+sensor  = dht.DHT11(machine.Pin(2))
+client = MQTTClient(CLIENT_ID, config.SERVER)
+
+def startup():
+    pass
+
+def handle_message(topic, msg):
+
+    print(topic, msg)
+
+    if topic != config.STATE_TOPIC:
+        return
+
+next_update = 0
+def loop():
+    global next_update
+
+    if next_update and next_update < time():
+        try:
+            sensor.measure()
+        except OSError as err:
+            print("Cannot read sensor:", err)
+
+        next_update += 5
+
+        payload = json.dumps({ 
+                "temperature": sensor.temperature(), 
+                "humidity": sensor.humidity() 
+            })
+        client.publish(config.STATE_TOPIC, payload)
+        print(payload)
+
+#    client.check_msg()
+#    if not state:
+#        return
+
+
+def setup():
+    global next_update
+
+    startup()
+
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect(config.WIFI_SSID, config.WIFI_PASSWORD)
+    print("connecting to wifi....")
+    while not sta_if.isconnected():
+        sleep(1)
+
+    print("Connected with IP", sta_if.ifconfig()[0])
+
+    client.set_callback(handle_message)
+    client.connect()
+    client.publish(config.DISCOVER_TOPIC_TEMP, 
+        json.dumps({
+            "name": config.NAME_TEMP, 
+            "device_class": "sensor",
+            "state_topic": config.STATE_TOPIC
+        }))
+    client.publish(config.DISCOVER_TOPIC_HUM, 
+        json.dumps({
+            "name": config.NAME_HUM, 
+            "device_class": "sensor",
+            "state_topic": config.STATE_TOPIC
+        }))
+
+    next_update = time()
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
